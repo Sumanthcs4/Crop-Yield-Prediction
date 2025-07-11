e2ac70f242bb095e9591e5a27fa2cc5be9f2aec7
Sumanthcs4 <ssumanth510@gmail.com>
2025-07-11 17:34:09 +0530

    setup.py is ready

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements()->List[str]:
+    requirement_lst:List[str]=[]
+    """
+    This function will return the list of requirements
+    """
+    try:
+        with open('requirements.txt') as file:
+            #Read lines fromm the file
+            lines = file.readlines()
+            # Process each linne
+            for line in lines:
+                requirement = line.strip()
+                #ignore the empty lines and -e .
+                if requirement and requirement!= '-e .':
+                    requirement_lst.append(requirement)
+                    
+    except FileNotFoundError:
+        print("requirements.txt file not found. Please create it with the required packages.")
+        
+    return requirement_lst
+
+setup(
+    name = "Crop-Yield-Prediction",
+    version = "0.0.1",
+    author= "Sumanth C S",
+    Email = "ssumanth510@gmail.com",
+    packages= find_packages(),
+    install_requires = get_requirements(),
+)
